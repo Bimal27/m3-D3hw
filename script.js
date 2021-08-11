f4e4f4e3f9151877577f88c49dedd16224d066f0
BananaKun <yanxun951224@gmail.com>
2021-08-11 15:57:19 +0100

    update

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
 
-        // Starting from the current "base" bootstrap layout, implement the following exercise:
+// Starting from the current "base" bootstrap layout, implement the following exercise:
 
-        // 1) When pressing on Load Images button, load the pictures from http://www.splashbase.co/api/v1/images/search?query=your query
-        // 2) When pressing on Load Seconday Images, load the pictures from http://www.splashbase.co/api/v1/images/search?query=your secondary query
-        // 3) When the user clicks on the "VIEW" button inside the Card, open the specified image in a modal view
-        // 4) The Edit button should be replace with a "Hide" button. 
-        // 5) When the hide button is pressed, the whole picture card disappears.
-        // 6) Replace the "9 mins" string in the card template with the ID of the Image
-
-
-       const showModal = (e) => {
-           const url = e.target.closest(".card").children[0].src
-           const body= document.querySelector('.modal-body')
-           image=`<img src="${url}"/>`
-           body.innerHTML= image
-       }
-       const hideImages = (e) =>{
-           e.target.closest('.col-md-4').remove()
-       }
+// 1) When pressing on Load Images button, load the pictures from http://www.splashbase.co/api/v1/images/search?query=your query
+// 2) When pressing on Load Seconday Images, load the pictures from http://www.splashbase.co/api/v1/images/search?query=your secondary query
+// 3) When the user clicks on the "VIEW" button inside the Card, open the specified image in a modal view
+// 4) The Edit button should be replace with a "Hide" button. 
+// 5) When the hide button is pressed, the whole picture card disappears.
+// 6) Replace the "9 mins" string in the card template with the ID of the Image
 
 
-        
-        const displayImages = (data) =>{
-            const row = document.querySelector('.album .row')
-            row.innerHTML = ""
+const showModal = (e) => {
+  const url = e.target.closest(".card").children[0].src
+  const body = document.querySelector('.modal-body')
+  image = `<img src="${url}"/>`
+  body.innerHTML = image
+  showAlert("display images");
+}
+const hideImages = (e) => {
+  e.target.closest('.col-md-4').remove()
+  showAlert("images has been delete");
+}
 
-            data.map((photo)=>{
-                row.innerHTML +=`
+
+
+const displayImages = (data) => {
+  const row = document.querySelector('.album .row')
+  row.innerHTML = ""
+
+  data.map((photo) => {
+    row.innerHTML += `
 
                 <div class="col-md-4">
                 <div class="card mb-4 shadow-sm">
@@ -56,34 +58,65 @@
                       <small class="text-muted">${photo.id}</small>
                     </div>
                   </div>`
-            })
+  })
 
-        }
-      
-        const loadImages = (query) => {
-            fetch(`https://api.pexels.com/v1/search?query=${query}`, {
-                "method": "GET",
-                "headers": {
-                    "Authorization": "Bearer 563492ad6f917000010000010ac492b0656442d1933d76c56ff8833e", 
-                }
-        
-            })
-                .then(response => response.json())
-                .then(data => {
-                    displayImages(data.photos)
-                }).catch(err => console.log(err))
-        }
+}
 
-        window.onload = () =>{
-             const primaryButton = document.querySelector('.btn-primary')
-             const secondaryButtonn = document.querySelector('.btn-secondary')
 
-             primaryButton.addEventListener('click' ,  () =>{
-                 loadImages('human')
-             })
-             secondaryButtonn.addEventListener('click', () =>{
-                 loadImages('nature')
-             })
-            
 
-        }
+const loadImages = (query) => {
+  fetch(`https://api.pexels.com/v1/search?query=${query}`, {
+    "method": "GET",
+    "headers": {
+      "Authorization": "Bearer 563492ad6f917000010000010ac492b0656442d1933d76c56ff8833e",
+    }
+
+  })
+    .then(response => response.json())
+    .then(data => {
+      displayImages(data.photos)
+      console.log(data)
+    }).catch(err => console.log(err))
+}
+
+const showAlert = (data) => {
+  const alert = document.querySelector('.alert.alert-primary ');
+  alert.innerText = data
+  alert.setAttribute("class", "alert alert-primary show")
+  setTimeout(() => alert.setAttribute("class", "alert alert-primary"), 5000);
+
+}
+
+const displayImgJum = (query) => {
+  fetch(`https://api.pexels.com/v1/search?query=${query}`, {
+    "method": "GET",
+    "headers": {
+      "Authorization": "Bearer 563492ad6f917000010000010ac492b0656442d1933d76c56ff8833e",
+    }
+
+  })
+    .then(response => response.json())
+    .then(data => {
+      const jum = document.querySelector('.jumbotron.jumbotron-fluid.container .container')
+      jum.innerHTML = `<img src="${data.photos.src.tiny}"/>`
+      console.log(data.photos)
+
+    }).catch(err => console.log(err))
+}
+
+
+window.onload = () => {
+  const primaryButton = document.querySelector('.btn-primary')
+  const secondaryButtonn = document.querySelector('.btn-secondary')
+
+  primaryButton.addEventListener('click', () => {
+    loadImages('human')
+    showAlert("9 human images has been load");
+  })
+  secondaryButtonn.addEventListener('click', () => {
+    loadImages('nature')
+    showAlert("9 nature images has been load");
+  })
+
+
+}
